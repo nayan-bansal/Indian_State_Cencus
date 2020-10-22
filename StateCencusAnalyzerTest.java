ce94e7dbf7a68913d2a917e27062c7dbf3eab4be
nayan-bansal <nayan.7991@gmail.com>
2020-10-23 03:35:20 +0530

    Added: Junit Test Case to Check File Format

--- StateCencusAnalyzerTest.java
+++ StateCencusAnalyzerTest.java
@@ -7,8 +7,12 @@ import junit.framework.Assert;
 
 public class CencusAnalyzerTest {
 
-private static String INDIA_CENSUS_CSV_FILE_PATH = "D:\\Capgemini Training\\CG Projects\\IndianStatesCensus\\StateCensus.csv";
-	
+private static String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\AAYUSH\\eclipse-workspace\\India_Cencus_Problem\\StateCensus.csv";
+private static String WRONG_CSV_FILE_PATH = "C:\\Users\\AAYUSH\\eclipse-workspace\\StateCensus.csv";
+private static String WRONG_CSV_FILE_TYPE = "C:\\Users\\AAYUSH\\eclipse-workspace\\India_Cencus_Problem\\StateCensus.java";	
+
+
+
 	@Test
     public void numberOfRecordsTest() {
         try {
@@ -30,4 +34,15 @@ private static String INDIA_CENSUS_CSV_FILE_PATH = "D:\\Capgemini Training\\CG P
 		}
 	}
 	
+	@Test
+	public void incorrectFileTypeTest() {
+		try {
+			StateCencusAnalyzer censusAnalyser = new StateCencusAnalyzer();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CencusAnalyzerException.class);
+			censusAnalyser.readCensusData(WRONG_CSV_FILE_TYPE);
+		} catch (CencusAnalyzerException e) {
+			Assert.assertEquals(CencusAnalyzerException.ExceptionType.CENSUS_FILE_EXCEPTION, e.type);
+		}
+	}
 }
